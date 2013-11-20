60975c8aadf887ba3d7029927b2d336628db8dfc
Mmil09 <Mmil09@gmail.com>
2013-11-19 19:54:02 -0500

    Testing

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 // -------------- Functions and Variables
 
+$.getJSON("http://otter.topsy.com/urlinfo.js?url=http://www.nytimes.com", function(data) {console.log(data.response.oneforty)});
 
 // ------------ Start of program
 
